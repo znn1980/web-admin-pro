1dd7d598513e152c2cec81b39e9cff22f798c3b9
znn1980 <znn1980@163.com>
2026-01-24 17:01:19 +0800

    v1.0.0 ending

--- src/main/resources/static/modules/common.js
+++ src/main/resources/static/modules/common.js
@@ -37,19 +37,6 @@ layui.define(function (exports) {
         , asRate: function (rate) {
             return (rate * 100).toFixed(2)
         }
-        , asTree: function (id, items) {
-            const trees = [];
-            items.forEach(function (item) {
-                if (item.pid === id) {
-                    const child = common.asTree(item.id, items);
-                    if (child.length) {
-                        item.children = child;
-                    }
-                    trees.push(item);
-                }
-            });
-            return trees;
-        }
         , asCols: function (that, id) {
             const fields = layui.data(id || that.id);
             that.cols[0].forEach(function (col) {
@@ -61,29 +48,6 @@ layui.define(function (exports) {
                 const field = layui.$(this).prev()[0];
                 layui.data(id || that.id, {key: field.name, value: field.checked});
             });
-        }
-        , _asCols: function (id, fields, cols) {
-            let queries = 0.0;
-            switch (layui.admin.screen()) {
-                case 0://低于768px的屏幕
-                    queries = cols && cols.xs && cols.xs >= 0 && cols.xs <= 1 ? cols.xs : 0;
-                    break;
-                case 1://768px到992px之间的屏幕
-                    queries = cols && cols.sm && cols.sm >= 0 && cols.sm <= 1 ? cols.sm : 0.3;
-                    break;
-                case 2://992px到1200px之间的屏幕
-                    queries = cols && cols.md && cols.md >= 0 && cols.md <= 1 ? cols.md : 0.7;
-                    break;
-                case 3://高于1200px的屏幕
-                    queries = cols && cols.lg && cols.lg >= 0 && cols.lg <= 1 ? cols.lg : 1;
-                    break;
-                default:
-                    queries = cols && cols.xl && cols.xl >= 0 && cols.xl <= 1 ? cols.xl : 1;
-                    break;
-            }
-            layui.table.hideCol(id, fields.map(function (field, index) {
-                return {field: field, hide: index >= Math.round(fields.length * queries)};
-            }));
         }
     }
     exports('common', common);
