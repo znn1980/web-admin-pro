c8dddbeea1ad36aae953d677d43ed8b3011f4564
znn1980 <znn1980@163.com>
2026-04-04 12:43:03 +0800

    完善不同模型对于《深度思考》与《联网搜索》的设置参数

--- src/main/java/com/admin/web/controller/AiChatController.java
+++ src/main/java/com/admin/web/controller/AiChatController.java
@@ -44,7 +44,9 @@ public class AiChatController extends BaseController {
                 .options(OpenAiChatOptions.builder()
                         .extraBody(Map.of(
                                 "enable_thinking", chatRequest.enableThinking(),
-                                "enable_search", chatRequest.enableSearch()
+                                "thinking", Map.of("type", chatRequest.enableThinking() ? "enabled" : "disabled"),
+                                "enable_search", chatRequest.enableSearch(),
+                                "web_search", Map.of("enable", chatRequest.enableSearch())
                         )).build())
                 .advisors(a -> a.param(CONVERSATION_ID, chatRequest.conversationId()))
                 .stream().chatResponse();
