c73ad3a95380be4ab91bdaedfe60eac1d6510101
znn1980 <znn1980@163.com>
2026-05-10 20:16:48 +0800

    v1.2.0 ending

--- src/main/resources/static/modules/common.js
+++ src/main/resources/static/modules/common.js
@@ -3,30 +3,9 @@
  */
 
 layui.define(function (exports) {
-
-    const localName = 'web-admin-pro'
-        , localMore = layui.$('button.sys-query-more').data('localMore');
-    if (localData()[localMore]) {
-        layui.$('.sys-query-more').toggle();
-    }
-
-    layui.$('button.sys-query-more').on('click', function () {
-        layui.$('.sys-query-more').toggle();
-        localData({[localMore]: !localData()[localMore]});
-    });
-
-    layui.table.set({skin: 'line', loading: true});
-
-    function localData(settings, uid) {
-        if (uid) layui.data(localName, {key: 'uid', value: uid});
-        const local = layui.data(localName).uid;
-        const data = layui.data(localName)[local] || {};
-        if (settings) layui.data(localName, {key: local, value: {...data, ...settings}})
-        return data;
-    }
-
-    exports('common', {
-        req: function (url, type, data, callback) {
+    const common = {
+        LOCAL_STORAGE_NAME: 'web-admin-pro'
+        , req: function (url, type, data, callback) {
             const loading = layui.layer.load(2);
             layui.admin.req({
                 url: url, type: type, data: data ? typeof data !== 'string' ? JSON.stringify(data) : data : {}
@@ -51,11 +30,15 @@ layui.define(function (exports) {
             });
         }
         , data: function (settings, uid) {
-            return localData(settings, uid);
+            if (uid) layui.data(this.LOCAL_STORAGE_NAME, {key: 'uid', value: uid});
+            const local = layui.data(this.LOCAL_STORAGE_NAME).uid;
+            const data = layui.data(this.LOCAL_STORAGE_NAME)[local] || {};
+            if (settings) layui.data(this.LOCAL_STORAGE_NAME, {key: local, value: {...data, ...settings}})
+            return data;
         }
         , asDay: function (day) {
             const ms = day ? day * (1000 * 60 * 60 * 24) : 0;
-            return layui.util.toDateString(Date.now() + ms, 'yyyy-MM-dd');
+            return layui.util.toDateString((Date.now() + ms), 'yyyy-MM-dd');
         }
         , asAgo: function (ms) {
             const m = 1000 * 60, h = m * 60, d = h * 24;
@@ -64,17 +47,18 @@ layui.define(function (exports) {
         , asRate: function (rate) {
             return (rate * 100).toFixed(2)
         }
-        , asCols: function (that, id) {
-            const fields = localData()[id || that.id] || {};
-            that.cols[0].forEach(function (col) {
+        , asCols: function (data, id) {
+            const fields = this.data()[id || data.id] || {};
+            data.cols[0].forEach(function (col) {
                 if (col.field in fields) {
-                    layui.table.hideCol(that.id, {field: col.field, hide: fields[col.field]});
+                    layui.table.hideCol(data.id, {field: col.field, hide: fields[col.field]});
                 }
             });
-            that.elem.next().on('mousedown', 'input[lay-filter="LAY_TABLE_TOOL_COLS"]+', function () {
+            const that = this;
+            data.elem.next().on('mousedown', 'input[lay-filter="LAY_TABLE_TOOL_COLS"]+', function () {
                 const field = layui.$(this).prev()[0];
-                const fields = localData()[id || that.id] || {};
-                localData({[id || that.id]: {...fields, [field.name]: field.checked}});
+                const fields = that.data()[id || data.id] || {};
+                that.data({[id || data.id]: {...fields, [field.name]: field.checked}});
             });
         }
         , asUuid: function () {
@@ -90,15 +74,37 @@ layui.define(function (exports) {
             });
             return layui.lay.flatToTree(data, {parentKey: 'pid'});
         }
-        , asWhois: function (ip) {
+        , asWhois: function (ip, callback) {
             this.req(`${config.base}sys/whois?ip=${ip}`, 'GET', null, {
                 done: function (data) {
                     let whois = data.data;
                     whois = (whois.pro || whois.city || whois.region)
                         ? [whois.pro, whois.city, whois.region].join('') : whois.addr
-                    layui.layer.msg(whois);
+                    typeof callback === 'function' && callback(whois);
                 }
             });
         }
+    }
+
+    if (common.data()[layui.$('button.sys-query-more').data('queryMore')]) {
+        layui.$('.sys-query-more').toggle();
+    }
+
+    layui.$('button.sys-query-more').on('click', function () {
+        layui.$('.sys-query-more').toggle();
+        const data = layui.$(this).data('queryMore');
+        common.data({[data]: !common.data()[data]});
     });
+
+    layui.util.on({
+        Whois: function (elem) {
+            common.asWhois(elem.text(), (whois) => {
+                layui.layer.tips(whois, this);
+            });
+        }
+    });
+
+    layui.table.set({skin: 'line', loading: true});
+
+    exports('common', common);
 });
