170ca1bb5707db2e1a86620252d88992e7573eaa
znn1980 <znn1980@163.com>
2026-06-01 12:51:30 +0800

    v1.3.0 ending

--- src/main/java/com/admin/web/service/SysUploadService.java
+++ src/main/java/com/admin/web/service/SysUploadService.java
@@ -19,57 +19,65 @@ import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 /**
  * @author znn
  */
 @Service
 public class SysUploadService {
-    private final ConfigProperties.Upload upload;
+    private final static String SYS_DOWNLOAD_URL = "/sys/download/";
+    private final Path location;
+    private final List<MimeType> extensions;
 
     public SysUploadService(ConfigProperties configProperties) {
-        this.upload = configProperties.getUpload();
+        this.location = Paths.get(configProperties.getUpload().getLocation());
+        this.extensions = configProperties.getUpload().getExtensions();
     }
 
     public SysUpload upload(HttpServletRequest request, MultipartFile file) throws IOException {
-        if (file.isEmpty() || !this.hasUpload(file.getContentType())) {
+        if (file.isEmpty() || !this.hasFilename(file.getOriginalFilename())) {
+            throw new ServerResponseException("上传的文件中包含无效的文件路径！");
+        }
+        if (file.isEmpty() || !this.hasContentType(file.getContentType())) {
             throw new ServerResponseException("上传的文件中包含不支持的格式！");
         }
         String fileName = this.uploadFilename(file);
-        Files.createDirectories(Paths.get(this.upload.getLocation(), fileName).getParent());
-        this.transferTo(file, fileName);
+        Files.createDirectories(this.location.resolve(fileName).getParent());
+        file.transferTo(this.location.resolve(fileName));
         return new SysUpload(
-                String.format("%s/sys/download/%s", request.getContextPath(), fileName),
+                String.format("%s%s%s", request.getContextPath(), SYS_DOWNLOAD_URL, fileName),
                 file.getOriginalFilename()
         );
     }
 
     public Resource download(HttpServletRequest request) throws NoResourceFoundException {
         String fileName = this.downloadFilename(request);
-        Resource resource = new FileSystemResource(Paths.get(this.upload.getLocation(), fileName));
-        if (!resource.exists()) {
+        Resource resource = new FileSystemResource(this.location.resolve(fileName));
+        if (!this.hasFilename(fileName) || !resource.exists()) {
             throw new NoResourceFoundException(HttpMethod.GET, fileName);
         }
         return resource;
     }
 
-    public boolean hasUpload(String contentType) {
+    public boolean hasFilename(String fileName) {
+        return this.location.resolve(fileName).normalize().startsWith(this.location);
+    }
+
+    public boolean hasContentType(String contentType) {
         if (StringUtils.hasLength(contentType)) {
-            for (MimeType mimeType : this.upload.getExtensions()) {
+            for (MimeType mimeType : this.extensions) {
                 if (mimeType.includes(MimeType.valueOf(contentType))) {
                     return true;
                 }
             }
         }
         return false;
-    }
-
-    public void transferTo(MultipartFile file, String fileName) throws IOException {
-        file.transferTo(Paths.get(this.upload.getLocation(), fileName));
     }
 
     public String uploadFilename(MultipartFile file) {
@@ -82,7 +90,7 @@ public class SysUploadService {
 
     public String downloadFilename(HttpServletRequest request) {
         return URLDecoder.decode(
-                StringUtils.delete(request.getRequestURI(), "/sys/download/"),
+                StringUtils.delete(request.getRequestURI(), SYS_DOWNLOAD_URL),
                 StandardCharsets.UTF_8
         );
     }
